c47b29c54d4062ad879c1d46787b0e5730fb48d9
CloneRainbow <150621787+CloneRainbow@users.noreply.github.com>
2025-10-30 23:34:05 +0200

    Initialize Geomagnetic Pro 2025 module imports

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,20 @@
+# Модулі Geomagnetic Pro 2025
+from .config import Config
+from .wmm_loader import WMMLoader
+from .coordinates import CoordinateConverter
+from .calculator import Calculator
+from .map_manager import MapManager
+from .data_import import DataImporter
+from .data_export import DataExporter
+from .session_manager import SessionManager
 
+__all__ = [
+    'Config',
+    'WMMLoader', 
+    'CoordinateConverter',
+    'Calculator',
+    'MapManager',
+    'DataImporter',
+    'DataExporter',
+    'SessionManager'
+]
