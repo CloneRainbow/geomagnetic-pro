d0a8700128e6e73400acd10dd9b4ffd653a84f48
CloneRainbow <150621787+CloneRainbow@users.noreply.github.com>
2025-10-31 13:44:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ session = SessionManager(config)
 exporter = DataExporter()
 
 calculator = Calculator(session)
-map_manager = MapManager(session, exportet)
+map_manager = MapManager(session, exporter)
     
     # Заголовок додатку
     st.title("🧭 Geomagnetic Pro 2025")
